eb4557584285d2c194797bc886114b08f78f3a62
Chris <magio@hm.edu>
2018-11-22 14:47:59 +0100

    Fehlende Methoden ergänzt, nur noch Menu fehlt!

--- src/CursorList.h
+++ src/CursorList.h
@@ -11,13 +11,12 @@
 #include "Student.h"
 #include <tuple>
 
-
 class CursorIterator;
 using namespace std;
 // Gibt erste Position mit dem Wert value bzw. stop bei erfolgloser Suche.
 template<typename Iterator, typename T>
 Iterator find(Iterator start, Iterator stop, const T& value) {
-	for (Iterator it = start; it != stop;it++) {
+	for (Iterator it = start; it != stop; it++) {
 		if (*it == value) {
 			return it;
 		}
@@ -45,9 +44,6 @@ private:
 public:
 	// Initialisiert die Vor- und Nachfolger der EntryList.
 	void initList() {
-
-		entryList[0].prev = nullIndex;
-		// andere Schreibweise.
 
 		entryList[0].prev = nullIndex;
 		// bessere Schreibweise #readablility
@@ -113,23 +109,25 @@ public:
 
 	//füge neues Element am Anfang der Liste hinzu.
 	void push_front(const T& t) {
-		int tmp_free = entryList[start_free].next;
-		// Wenn schon Elemente in der Liste liegen!
+		int next_free = entryList[start_free].next;
 		if (!empty()) {
+			// Wenn schon Elemente in der Liste liegen!
 			entryList[start_free].next = start_list;
 			entryList[start_list].prev = start_free;
+		} else if (empty()) {
+			entryList[start_list].next = nullIndex;
 		}
+
 		entryList[start_free].data = t;
 		entryList[start_free].prev = nullIndex;
 		// Start der Listen neu setzen.
 		start_list = start_free;
-		start_free = tmp_free;
+		start_free = next_free;
 		entryList[start_free].prev = nullIndex;
 
 	}
 
 	void pop_front() {
-		delete entryList[start_list].data;
 		entryList[start_free].prev = start_list;
 		start_free = start_list;
 		entryList[start_free].prev = nullIndex;
@@ -139,80 +137,70 @@ public:
 
 	//Innere Klasse Cursor Iterator!!
 	class CursorIterator {
-			typedef CursorIterator iterator;
-		private:
-			int cursorIndex = -1;
-			CursorList<T, SIZE> &cursorList;
-		public:
+		typedef CursorIterator iterator;
+	private:
+		int cursorIndex = -1;
+		CursorList<T, SIZE> &cursorList;
+	public:
 
-			CursorIterator() {
-				cursorList = CursorList();
-			}
+		CursorIterator() {
+			cursorList = CursorList();
+		}
 
-			CursorIterator(CursorList<T, SIZE>& list)  : cursorList(list) {
-				cursorIndex = cursorList.getStart_list();
-			}
+		CursorIterator(CursorList<T, SIZE>& list) :
+				cursorList(list) {
+			cursorIndex = cursorList.getStart_list();
+		}
 
+		CursorList<T, SIZE> getCursorList() {
+			return this->cursorList;
+		}
 
-			CursorList<T, SIZE> getCursorList() {
-				return this->cursorList;
-			}
+		int getCursorIndex() {
+			return this->cursorIndex;
+		}
 
-			int getCursorIndex() {
-				return this->cursorIndex;
-			}
+		int getNextIteratorElement() {
+			return this->cursorList.getNextElement(cursorIndex);
+		}
 
-			int getNextIteratorElement() {
-				return this->cursorList.getNextElement(cursorIndex);
-			}
+		int getPrevIteratorElement() {
+			return this->cursorList.getPrevElement(cursorIndex);
+		}
 
-			int getPrevIteratorElement() {
-							return this->cursorList.getPrevElement(cursorIndex);
-			}
+		T& operator *() {
+			return this->cursorList.getListElement(cursorIndex);
+		}
 
-			T& operator *() {
-				return this->cursorList.getListElement(cursorIndex);
-			}
+		iterator& operator =(const iterator& rhs) {
+			this->cursorIndex = rhs.cursorIndex;
+			this->cursorList = rhs.cursorList;
+			return *this;
+		}
 
-			iterator& operator =(const iterator& rhs) {
-				this->cursorIndex = rhs.cursorIndex;
-				this->cursorList = rhs.cursorList;
-				return this;
-			}
+		bool operator !=(iterator& rhs) {
+			return this->operator *() != rhs.operator *();
+		}
+		bool operator ==(iterator& rhs) {
+			return this->operator *() == rhs.operator *();
+		}
 
-			bool operator !=(iterator& rhs) {
-				return this->operator *() != rhs.operator *();
-			}
-			bool operator ==(iterator& rhs) {
-				return this->operator *() == rhs.operator *();
-			}
+		iterator& operator++() {
+			cursorIndex = this->getNextIteratorElement();
+			return *this;
 
-			iterator& operator++() {
-				if (cursorList.getNextElement(cursorIndex) == nullIndex)
-					return nullptr;
+		}
+		iterator operator ++(int) {
+			cursorIndex = this->getNextIteratorElement();
+			return *this;
+		}
 
-				this->cursorIndex = this->cursorList.getNextElement();
-				return this;
+		virtual ~CursorIterator() {
 
-			}
-			iterator operator ++(int) {
-				if (cursorList.getNextElement(cursorIndex) == nullIndex) {
+		}
 
-				}
-
-				this->cursorIndex = this->cursorList.getNextElement(cursorIndex);
-				return *this;
-			}
-
-			virtual ~CursorIterator() {
-
-			}
-
-		};
+	};
 	typedef CursorIterator iterator;
-
-
-
 
 	iterator begin() {
 		return CursorIterator(*this);
@@ -229,79 +217,78 @@ public:
 
 	//insert befor itr
 	iterator insert(iterator itr, T value) {
-		//Falls die Liste noch leer ist oder der Iterator auf das erste Element Zeigt !
-		if(itr.getCursorIndex() == start_list) {
+		//testing
+		//cout << "hey there im insert" << endl << endl;
+
+		//Falls die Liste noch leer ist!
+		if (empty() || itr.getCursorIndex() == start_list) {
 			push_front(value);
+			//testing
+			//cout << "START LIST: " << start_list << endl;
+			//cout << "START FREE: " << start_free << " NEXT" << entryList[start_free].next << " IF-LOOP" << endl;
+			//toString();
+			return itr;
+		} else {
+			//neues Element an Start der Freelist einfügen.
+			int next_free = entryList[start_free].next;
+			entryList[itr.getPrevIteratorElement()].next = start_free;
+			entryList[start_free].data = value;
+			entryList[start_free].prev = itr.getPrevIteratorElement();
+			entryList[start_free].next = itr.getCursorIndex();
+
+			//Prev. des Iterator Elements ändern.
+			entryList[itr.getCursorIndex()].prev = start_free;
+
+			start_free = next_free;
+			entryList[start_free].prev = nullIndex;
+			//testing
+			//toString();
+			//cout << "START LIST: " << start_list << endl;
+			//cout << "START FREE: " << start_free << "NEXT: " << entryList[start_free].next << endl;
 			return itr;
 		}
-		//neues Element an Start der Freelist einfügen.
-		entryList[start_free].data = value;
-		entryList[start_free].prev = itr.getPrevIteratorElement();
-		entryList[start_free].next = itr.getCursorIndex();
-
-		//Prev. des Iterator Elements ändern.
-		entryList[itr.getCursorIndex()].prev = start_free;
-		start_free = entryList[start_free].next;
-		return itr;
 
 	}
-	iterator erase(iterator itr){
-
+	iterator erase(iterator itr) {
 		int tmp = itr.getCursorIndex();
-
 		entry erased_entry = itr.getCursorList().entryList[tmp];
-
-		//itr.getCursorList().entryList[tmp].data = s;
-
-		itr++;
-
-		entry tmp_entry = itr.getCursorList().entryList[erased_entry.next];
-
+		++itr;
 		entryList[erased_entry.next].prev = erased_entry.prev;
-
 		entryList[erased_entry.prev].next = erased_entry.next;
-
 		erased_entry.prev = -1;
 		erased_entry.next = start_free;
 		entryList[start_free].prev = tmp;
 		start_free = tmp;
-
 		entryList[start_free] = erased_entry;
 
-		//this is crap but i hate bananas
 		return itr;
-
-
-
-		//itr.cursorList.entryList[1];
-	};
-
-	iterator erase(iterator start, iterator stop);//stop exclusive
-
-//	string  toString() {
-//		stringstream tmp;
-//		int i = start_list;
-//		while (i != -1) {
-//			tmp << entryList[i].data << endl  ;
-//			i = entryList[i].next;
-//		}
-//		return tmp.str();
-//	}
-
-
-	void toString2(){
-		int counter = 0;
-		for(entry e : entryList){
-			cout << counter++ << ": " << e.data << " " << e.prev << " " << e.next << endl;
-			//4Testig delete lower lines
-			//if(counter == 6) break;
-		}
 	}
-//	void toString3(){
-//		for(auto it = this->begin();it!=this->end();++it){
-//			cout << *it << endl;
-//		}
-//	}
+
+	iterator erase(iterator start, iterator stop) {
+		iterator tmp = stop++;
+		entryList[start.getPrevIteratorElement()].next =
+				stop.getNextIteratorElement();
+		entryList[stop.getNextIteratorElement()].prev =
+				start.getPrevIteratorElement();
+		entryList[start.getCursorIndex()].prev = nullIndex;
+		entryList[start_free].prev = stop.getCursorIndex();
+		entryList[stop.getCursorIndex()].next = start_free;
+		start_free = start.getCursorIndex();
+		return tmp;
+	}
+
+	void toString() {
+		int counter = 0;
+		cout << "___________________________________________" << endl;
+		for (entry e : entryList) {
+			cout << counter++ << ": " << e.data << " " << e.prev << " "
+					<< e.next << endl;
+			//4Testig delete lower lines
+			if (counter == SIZE)
+				break;
+		}
+		cout << "___________________________________________" << endl;
+	}
 
 };
 #endif /* CURSORLIST_H_ */
--- src/Praktikumsaufgabe.cpp
+++ src/Praktikumsaufgabe.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Praktikumsaufgabe.cpp
-// Author      : Chris
+// Author      : Chris, Alex
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -16,31 +16,83 @@ using namespace std;
 int main() {
 	CursorList<Student, 20> li;
 	CursorList<Student, 20>::iterator it(li);
-	cout
-			<< "Bitte Name, Vorname, Martikelnummer(6-stellig) und Geburtsdatum(MMDDJJJJ) eingeben!"
-			<< endl;
-	cout << "Abbruch erfolgt durch Eingabe von Exit." << endl;
 
 	std::stringstream istrstr;
+	bool takeList = false;
+	bool correctInput = true;
 
-	for (int i = 0; i <= 8; ++i) {
+	cout << "For entering students type \"students\" " << endl;
+	cout << "For using a list of five students type \"list\" " << endl;
 
-		Student s;
-		//cin >> s;
-
+	while (correctInput) {
 		string tmp;
-		tmp = "";
-		getline(cin,tmp);
+		getline(cin, tmp);
+		cin.clear();
 
-		if(tmp == "Exit"){
-			break;
+		if (tmp == "students") {
+			cout << "Please type in Data in the following form: " << endl;
+			cout
+					<< "[Name, Surname, Martikelno. with 6 digets, birthdate in DDMMJJJJ]"
+					<< endl;
+
+			cout << "If you are finished adding students type \"exit\"" << endl;
+			takeList = false;
+			correctInput = false;
+		} else if (tmp == "list") {
+			takeList = true;
+			correctInput = false;
+		} else {
+			cout << "Your input was wrong! Are you sure " << tmp
+					<< " is correct? Try again..." << endl;
 		}
-		istrstr.str(tmp);
-		istrstr >> s;
-
-		li.insert(it, s);
-		istrstr.clear();
 	}
+
+	if (takeList) {
+		string name[] = { "Alex eins ", "Thomas zwei ", "Paul drei ",
+				"Heinrich vier ", "hennry fuenf " };
+		for (int i = 0; i < 5; i++) {
+			Student s;
+			string tmp = "";
+			tmp.append(name[i]);
+			string index = std::to_string(i + 1);
+			tmp.append(index);
+			tmp.append(" ");
+			tmp.append(index);
+
+			istrstr.str(tmp);
+			istrstr >> s;
+			li.insert(it, s);
+			istrstr.clear();
+		}
+	} else {
+		for (int i = 0; i <= 20; ++i) {
+
+			Student s;
+			string tmp;
+			tmp = "";
+			getline(cin, tmp);
+
+			if (tmp == "exit") {
+				break;
+			}
+
+			if (tmp != "show") {
+				istrstr.str(tmp);
+				istrstr >> s;
+				li.insert(it, s);
+				istrstr.clear();
+			}
+			if (tmp == "show") {
+				li.toString2();
+				istrstr.clear();
+			}
+
+		}
+	}
+
+	cout << "The List is ready!!" << endl;
+	cout << "The following commands are available" << endl;
+	cout << "";
 
 	Student sFind("Chris", "Magios", 123456, 27021996);
 	//cout << "Start "<< li.getStart_list() << "free "<< li.getStart_free() << endl;
@@ -61,9 +113,5 @@ int main() {
 	//Harry Sixed 115599 98653247
 	li.toString2();
 	li.erase(it);
-
-	cout << "~~~~~~~~~~~~~AFTER ERASED~~~~~~~~~~~~~~~~~~~~~~" << endl;
-	li.toString2();
-
 
 }
